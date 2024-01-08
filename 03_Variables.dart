c84982d4b54761c7e15405b38d60992b310472d0
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-08 23:53:39 +0530

    variables

---
+++ 03_Variables.dart
@@ -0,0 +1,31 @@
+void main()
+{
+    //like java these work
+    int a = 100;
+    String s = "hello";
+    bool b = true; //not like java boolean only uses bool
+
+    //var like javascript works
+    var v0 = 100;
+    var v1 = "hello";
+    var v2 = true;
+
+    //const like javascript works
+    const v3 = 100;
+    const v4 = "hello";
+    const v5 = true;
+
+    //let like js doesnt work 
+    //primitives like Integer, Boolean, Float doesnt work
+    //like python directly it doesnt work because it is statically typed
+
+    print(a);
+    print(s);
+    print(b);
+    print(v0);
+    print(v1);
+    print(v2);
+    print(v3);
+    print(v4);
+    print(v5);
+}
