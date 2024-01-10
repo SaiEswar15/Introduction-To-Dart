0a656d555abab864f9a4bb2caba63f46b9db4617
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-10 23:38:29 +0530

    positional args and parms errors you may make

--- 05_NamedFunctions.dart
+++ 05_NamedFunctions.dart
@@ -9,10 +9,32 @@
 //     print("normal function");
 // }
 
+// void main()
+// {
+//     print("hello world");
+//     normalFunction('hai', 25, 'eswar');
+// }
+
+// void normalFunction(String greetings, int age, String name)
+// {
+//     print('$greetings I am $name of age $age');
+// }
+
 void main()
 {
     print("hello world");
-    normalFunction('hai', 25, 'eswar');
+    normalFunction('eswar', 25, 'hai');
+}
+
+void normalFunction(String greetings, int age, String name)
+{
+    print('$greetings I am $name of age $age');
+}
+
+void main()
+{
+    print("hello world");
+    normalFunction('eswar', 'hai', 25);
 }
 
 void normalFunction(String greetings, int age, String name)
