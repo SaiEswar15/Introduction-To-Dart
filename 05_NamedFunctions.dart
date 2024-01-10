234e168c947619d5c99750772890dbdaa8295e3c
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-10 23:40:59 +0530

    named args and params

--- 05_NamedFunctions.dart
+++ 05_NamedFunctions.dart
@@ -20,24 +20,35 @@
 //     print('$greetings I am $name of age $age');
 // }
 
+// void main()
+// {
+//     print("hello world");
+//     normalFunction('eswar', 25, 'hai');
+// }
+
+// void normalFunction(String greetings, int age, String name)
+// {
+//     print('$greetings I am $name of age $age');
+// }
+
+// void main()
+// {
+//     print("hello world");
+//     normalFunction('eswar', 'hai', 25);
+// }
+
+// void normalFunction(String greetings, int age, String name)
+// {
+//     print('$greetings I am $name of age $age');
+// }
+
 void main()
 {
     print("hello world");
-    normalFunction('eswar', 25, 'hai');
+    normalFunction(age : 25, greetings : 'hello', name : 'eswar');
 }
 
-void normalFunction(String greetings, int age, String name)
-{
-    print('$greetings I am $name of age $age');
-}
-
-void main()
-{
-    print("hello world");
-    normalFunction('eswar', 'hai', 25);
-}
-
-void normalFunction(String greetings, int age, String name)
+void normalFunction({required String name,required String greetings,required int age})
 {
     print('$greetings I am $name of age $age');
 }
