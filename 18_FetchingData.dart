57df6a5ba74ddee181d4c9b91cc0c99fe8143bab
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-14 15:38:07 +0530

    fetching data in dart and packages intro

---
+++ 18_FetchingData.dart
@@ -0,0 +1,19 @@
+// import "package:http/http.dart" as http;
+// //should configure in yaml project
+// import "dart:convert";
+
+
+// void main() async
+// {
+  
+//   var url = Uri.https("flipkart-version3.onrender.com", "/api/v1/products");
+//   try
+//   {
+//     final response = await http.get(url);
+//     print(jsonDecode(response.body)[0]["Title"]);
+//   }
+//   catch(error)
+//   {
+//     print("failed to get data from api");
+//   }
+// }
